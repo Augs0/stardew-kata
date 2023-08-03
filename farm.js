310115e01a13cd52530e1c6a2d176babad3e5ece
August <34795090+Augs0@users.noreply.github.com>
2023-08-03 10:47:32 +0100

    Add boilerplate code in js file

--- farm.js
+++ farm.js
@@ -1,1 +1,12 @@
-// your code here
+// Level 1
+function cropWateringCalculator(forecast){
+  
+}
+
+// Level 2 and 3
+function cropWateringCalculatorImproved(forecast, numOfCrops){
+
+}
+
+module.exports = { cropWateringCalculator, cropWateringCalculatorImproved }
+
